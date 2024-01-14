e083659e3631efb510e53159c14928523bacec1e
Henrique Vieira da Silva <henrique.vieira2005@outlook.com>
2024-01-14 16:01:19 -0300

    1.1.0 henriquevieiradasilva.com.br
     Troquei os comentários, tirei deixei mais minimalista.

--- script.js
+++ script.js
@@ -1,10 +1,6 @@
-// Exemplo de script JavaScript simples
 document.addEventListener("DOMContentLoaded", function() {
     console.log("O script foi carregado com sucesso!");
 });
 document.addEventListener("DOMContentLoaded", function() {
-    // Código a ser executado quando o DOM estiver completamente carregado
-
-    // Exibe uma caixa de diálogo de boas-vindas
     alert("Bem-vindo ao meu site!");
 });
