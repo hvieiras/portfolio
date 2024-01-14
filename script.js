466838b15e1aafb9feb3dc229b8eccdc09079286
Henrique Vieira da Silva <henrique.vieira2005@outlook.com>
2024-01-14 16:03:29 -0300

    1.1.1 henriquevieiradasilva.com.br
     Tirei parte inutilizada do javascript

--- script.js
+++ script.js
@@ -1,6 +1,3 @@
 document.addEventListener("DOMContentLoaded", function() {
-    console.log("O script foi carregado com sucesso!");
-});
-document.addEventListener("DOMContentLoaded", function() {
     alert("Bem-vindo ao meu site!");
 });
